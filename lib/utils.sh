cae47c43fa473362eac645b46ca192cdb5555e32
Chris Rode <chris@hooroo.com>
2017-08-04 00:27:41 +1000

    adding the new location for the utils

---
+++ lib/utils.sh
@@ -0,0 +1,23 @@
+ensure_rbuild_installed(){
+	if [ ! -f "$(rbuild_command)" ]; then
+		download_rbuild 1>&2
+	fi
+}
+
+download_rbuild(){
+	echo "Downloading R-env..."
+	git clone "https://github.com/cirode/R-build.git" "$(rbuild_directory_path)"
+  local old_path="$(pwd)"
+  cd "$(rbuild_directory_path)"
+  git checkout bug/compilation_issues #Open pull request at https://github.com/viking/R-build/pull/8
+  cd $old_path
+}
+
+rbuild_command(){
+	echo "$(rbuild_directory_path)/bin/R-build"
+}
+
+rbuild_directory_path(){
+	echo "$(dirname $(dirname $0))/R-build"
+}
+
