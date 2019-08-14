e30029dab0b02cd5140cc26d59ae868223a1fa39
Zura Guerra <guerrazuramx@gmail.com>
2019-08-14 14:06:39 -0500

    Use most updated R-builder fork

--- lib/utils.sh
+++ lib/utils.sh
@@ -6,7 +6,7 @@ ensure_rbuild_installed(){
 
 download_rbuild(){
 	echo "Downloading R-build..."
-	git clone "https://github.com/cirode/R-build.git" "$(rbuild_directory_path)"
+	git clone "https://github.com/viking/R-build.git" "$(rbuild_directory_path)"
   local old_path="$(pwd)"
   cd "$(rbuild_directory_path)"
   git checkout bug/compilation_issues #Open pull request at https://github.com/viking/R-build/pull/8
@@ -20,4 +20,3 @@ rbuild_command(){
 rbuild_directory_path(){
 	echo "$(dirname $(dirname $0))/R-build"
 }
-
